82a3ec1eaac43688ada469f1e5b010dba93cd03b
Scott Lewis <scott@roof12.com>
2025-09-22 14:30:22 -0600

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- kj to exit insert mode
+vim.keymap.set("i", "kj", "<ESC>", { silent = true })
+
+-- backspace goes to previous file
+vim.keymap.set("n", "<bs>", ":edit #<cr>", { silent = true })
